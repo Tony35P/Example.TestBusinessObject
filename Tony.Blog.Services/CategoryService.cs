dfeccb72597589a56095bf139566281425f2cec7
Tony_Tsai <tonytsai27@gmail.com>
2022-03-05 22:29:16 +0800

    這支程式的重點在: 每次叫用Create, 都有做到驗證判斷是否資料是否重複, 存取資料庫反而不需要測,

--- Tony.Blog.Services/CategoryService.cs
+++ Tony.Blog.Services/CategoryService.cs
@@ -11,29 +11,53 @@ namespace Tony.Blog.Services
 {
     public class CategoryService
     {
-        private string connString;
-        public CategoryService(string connString)
+        private ICategoryDAO dao;
+        public CategoryService(ICategoryDAO dao)
         {
-            this.connString = connString;
+            this.dao = dao;
         }
 
         public void Create(string categoryName)
+        {
+            if(dao.IsExist(categoryName))
+            {
+                throw new Exception("相同的名稱已存在");
+            }
+            dao.Create(categoryName);
+        }
+    }
+    public interface ICategoryDAO
+    {
+        void Create(string categoryName);
+
+        bool IsExist(string categoryName);
+    }
+    public class CategoryDAO : ICategoryDAO
+    {
+        private string connString;
+        public CategoryDAO(string connString)
+        {
+            this.connString=connString;
+        }
+        public void Create(string categoryName)
+        {
+            using (var conn = new SqlConnection(connString))
+            {                
+                var sql = "INSERT INTO Categories(CategoryName)VALUES(@CategoryName)";
+                conn.Execute(sql, new { CategoryName = categoryName });
+            }
+        }
+
+        public bool IsExist(string categoryName)
         {
             var sql = "SELECT Id FROM Categories WHERE CategoryName=@CategoryName";
 
             using (var conn = new SqlConnection(connString))
             {
                 var result = conn.QueryFirstOrDefault(sql, new { CategoryName = categoryName });
-                if (result != null)
-                {
-                    throw new Exception("相同的分組名稱已存在");
-                }
-
-                sql = "INSERT INTO Categories(CategoryName)VALUES(@CategoryName)";
-                conn.Execute(sql, new { CategoryName = categoryName });
+               
+                return (result != null);
             }
         }
     }
-
-    
 }
